e647df0486120a0f728391ed74dcb1c56f80ec78
brainyfarm <brainyfarm@gmail.com>
2019-03-18 21:46:29 +0100

    add address json

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 $(document).ready(() => {
-
   $('#address-form').on('submit', (e) => {
     e.preventDefault();
     const baseUrl = 'https://us-street.api.smartystreets.com/street-address?auth-id=149cff65-035e-5a1b-7204-e59b0be4ed54&auth-token=19437337625682759&';
     const queries = $('#address-form').serialize();
     const requestUrl = `${baseUrl}${queries}`;
     $.getJSON(requestUrl, response => {
-      console.log(JSON.stringify(response));
-
       if ( !response.length ) {
         alert('Unable to validate address');
       } else {
@@ -20,6 +17,7 @@ $(document).ready(() => {
         $('#city').val(response[0].components.city_name);
         $('#zipcode').val(response[0].components.zipcode + '-' + response[0].components.plus4_code);
         $('#state').val(response[0].components.state_abbreviation);
+        $('#json').JSONView(response);
       }
     });
   })
