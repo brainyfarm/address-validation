1444a71f7a1a31166f0a4009df373823d99a5801
brainyfarm <brainyfarm@gmail.com>
2019-03-25 17:38:57 +0100

    add country field for internation validation

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(document).ready(() => {
+$(document).ready(() => { 
   $('#address-form').on('submit', (e) => {
     e.preventDefault();
     const baseUrl = 'https://us-street.api.smartystreets.com/street-address?auth-id=149cff65-035e-5a1b-7204-e59b0be4ed54&auth-token=19437337625682759&';
