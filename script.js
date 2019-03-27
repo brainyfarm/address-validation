50710b39dc9dcf8d90139714e584aa8cf05751c5
brainyfarm <brainyfarm@gmail.com>
2019-03-27 12:56:12 +0100

    add new key

--- script.js
+++ script.js
@@ -3,24 +3,30 @@ $(document).ready(() => {
     e.preventDefault();
     const clientKey = '24452420731202627';
     const authID = 'd73d3614-f1d3-f9f6-d7df-7d7414ad4895';
-    const baseUrl = `https://us-street.api.smartystreets.com/street-address?auth-id=${authID}&auth-token=${clientKey}&`;
-    const queries = $('#address-form').serialize();
-    const requestUrl = `${baseUrl}${queries}`;
-    $.getJSON(requestUrl, response => {
-      if ( !response.length ) {
-        alert('Unable to validate address');
-      } else {
-        $('#valid').fadeIn('2000', () => {
-          setTimeout(() => {
-            $('#valid').fadeOut('slow');
-          }, 3000);
-        });
-        $('#street').val(response[0].delivery_line_1);
-        $('#city').val(response[0].components.city_name);
-        $('#zipcode').val(response[0].components.zipcode + '-' + response[0].components.plus4_code);
-        $('#state').val(response[0].components.state_abbreviation);
-        $('#json').JSONView(response);
-      }
-    });
+    const authInfo = `auth-id=${authID}&auth-token=${clientKey}`;
+    const queriesUS  = $('#address-form').serialize();
+    const baseUrlUS = `https://us-street.api.smartystreets.com/street-address?${authInfo}&`;
+    const requestUrlUS = `${baseUrlUS}${queriesUS}`;
+    const formData = {};
+    $('#address-form').serializeArray().map(input => { formData[input.name]  = input.value });
+
+    if ( formData.country === 'United States' )  {
+      $.getJSON(requestUrlUS, response => {
+        if ( !response.length ) {
+          alert('Unable to validate address');
+        } else {
+          $('#valid').fadeIn('2000', () => {
+            setTimeout(() => {
+              $('#valid').fadeOut('slow');
+            }, 3000);
+          });
+          $('#street').val(response[0].delivery_line_1);
+          $('#city').val(response[0].components.city_name);
+          $('#zipcode').val(response[0].components.zipcode + '-' + response[0].components.plus4_code);
+          $('#state').val(response[0].components.state_abbreviation);
+          $('#json').JSONView(response);
+        }
+      });
+    }
   })
 });
